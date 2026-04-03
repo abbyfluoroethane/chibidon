ab87611219af75dab6abbf0dbea4e9461021c599
Abby Bigaouette <abby@bigaouette.com>
2026-04-03 00:16:48 -0400

    Fix round screen padding on post detail screen

--- app/src/main/java/org/chibidon/ui/screens/StatusDetailScreen.kt
+++ app/src/main/java/org/chibidon/ui/screens/StatusDetailScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -61,7 +62,9 @@ fun StatusDetailScreen(
 		TransformingLazyColumn(
 			state = columnState,
 			contentPadding = contentPadding,
-			modifier = Modifier.fillMaxSize(),
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(horizontal = 10.dp),
 		) {
 			when (val state = uiState) {
 				is StatusDetailUiState.Loading -> {
